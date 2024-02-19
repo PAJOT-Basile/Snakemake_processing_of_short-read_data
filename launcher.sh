708217f8274cc3c20c8a4f13a3d4bd21537baa9b
PAJOT-Basile <156067026+PAJOT-Basile@users.noreply.github.com>
2024-02-19 14:32:51 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task 8
 
 # Load the necessary modules
-module load snakemake/7.25.0 fastqc/0.12.1 multiqc/1.13 fastp/0.23.1 samtools/1.15.1 bwa/0.7.17 bcftools/1.16 vcftools/0.1.16 multiqc/1.13 vcftools/0.1.16 
+module load snakemake/7.25.0 fastqc/0.12.1 multiqc/1.13 fastp/0.23.1 samtools/1.15.1 bwa/0.7.17 bcftools/1.16 vcftools/0.1.16 multiqc/1.13 bcftools/0.1.16 
 
 # Remove locking files
 rm -rf .snakemake/incomplete/* .snakemake/locks/* .snakemake/tmp.*
