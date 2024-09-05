dff3ee746836afed383b03d309021d9115082ffd
PAJOT-Basile <156067026+PAJOT-Basile@users.noreply.github.com>
2024-09-05 16:34:23 +0200

    Library update
    Help to load libraries if needed

--- Scripts_snk/Graph_quality.r
+++ Scripts_snk/Graph_quality.r
@@ -1,16 +1,25 @@
 # Install libraries if needed and load them
+libraries <- c("tidyverse", "ggpubr", "argparse")
 tryCatch(
-    expr = {
-        install.packages(setdiff("anyLib", rownames(installed.packages())))
-        library(anyLib)
-        anyLib(c("tidyverse", "ggpubr", "argparse"))
-    },
-    error = function(e){
-        install.packages(setdiff(c("tidyverse", "ggpubr", "argparse"), rownames(installed.packages())))
-        library(tidyverse); library(ggpubr); library(argparse)
+  expr = {
+    install.packages(setdiff("anyLib", rownames(installed.packages())))
+    library(anyLib)
+    anyLib(libraries)
+  },
+  error = function(e){
+    install.packages(setdiff(libraries, rownames(installed.packages())))
+    for (libr in libraries){
+      tryCatch(
+        expr = {
+          library(libr)
+        },
+        error = function(e){
+          library(libr, lib.loc = "~/R/x86_64-conda-linux-gnu-library/4.2/")
+        }
+      )
     }
+  }
 )
-
 
 # Take inputs from the snakemake program
 parser <- ArgumentParser(description = "This program is used to create a table of sites to keep on the filtration on Strand Bias")
